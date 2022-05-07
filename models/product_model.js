1bbf29337a68b47ac6b95a4c705b73d59cbab816
rkirk430 <r.kirk430@gmail.com>
2022-05-06 21:45:00 -0400

    Models folder set up

---
+++ models/product_model.js
@@ -0,0 +1,25 @@
+//models/product_model.js
+
+const products = [
+    {
+      name: 'Berry Kombucha',
+      price: 8,
+      image:
+        'https://cdn.shopify.com/s/files/1/1297/1509/products/hero1_6de889fb-b540-49e4-b733-3af0baaa7f63_x1440.jpg?v=1571274629',
+    },
+    {
+      name: 'Mango Kombucha',
+      price: 11,
+      image:
+        'https://cdn.shopify.com/s/files/1/1297/1509/products/HERO_c5b0ec76-ad06-4cc7-a165-6129e11a8ff6_x1440.jpg?v=1571274622',
+    },
+    {
+      name: 'White Peach Kombucha',
+      price: 7,
+      image:
+        'https://cdn.shopify.com/s/files/1/1297/1509/products/hero1_40030160-f468-4d50-8f30-c8b9733ce84e_x1440.jpg?v=1575020412',
+    },
+  ];
+
+  module.exports = products
+  
--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const app = express();
 const PORT = 5000;
 
 //Temporary Database
-const products = ['berry kombucha', 'mango kombucha', 'white peach kombucha']
 
-// Routes / Controller
+
+//=============================================================================================
+                        //Routes/Controllers
+//=============================================================================================
+
+//Index Route - catches GET requests to /products/ and responds with ALL products
+app.get('/products/', (req,res) => {
+    const context = { products: products };
+    res.render('index.ejs', context);
+})
+
+
+//Show Route - catches GET requests to /products/index/ and respond with a SINGLE product
 app.get('/products/:productIndex', (req,res) => {
     res.send(products[req.params.productIndex]);
 });
