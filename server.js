cc6122f1e734cf8e85945adf81b3096f0d66ceda
rkirk430 <r.kirk430@gmail.com>
2022-05-06 21:31:33 -0400

    Route Params Set

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+//Temporary Database
+const products = ['berry kombucha', 'mango kombucha', 'white peach kombucha']
+
+// Routes / Controller
+app.get('/products/:productIndex', (req,res) => {
+    res.send(products[req.params.productIndex]);
+});
+
+//http://localhost:5000/products/1
+
 app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT}`));
