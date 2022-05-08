1533c1db4a1fe31dbc2290f2fccd8d54662c386c
rkirk430 <r.kirk430@gmail.com>
2022-05-08 19:27:48 -0400

    Issues with Controllers

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,6 +1,79 @@
 // controllers/products_controller.js
 
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+//Simulated Database
+const products = require('../models/product_model')
 
-module.exports = router;
+// request http://localhost:4000/products
+
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+
+// Products "show" route - GET - one product 
+
+router.get('/:id/', (req, res) => {
+    let productId = req.params.id
+
+    const context = {
+        oneProduct: products[productId],
+        message: 'I am the show route',
+        id: productId
+    }
+    res.render('show.ejs', context)
+})
+
+
+// Products "edit" route - GET - display an edit form for one product
+
+router.get('/:id/edit', (req,res)=>{
+    const foundProduct = products[req.params.id]
+    const context = {
+        product: foundProduct,
+        id: req.params.id
+    }
+    res.render('edit.ejs', context)
+})
+
+
+// Product "index" route - GET - all products
+
+router.get('/', (req, res) => {
+    // res.send(products)
+    const context = { products }
+    res.render('index', context)
+})
+
+
+// Products "create" route - POST requests -> request body (new product data)
+
+router.post('/', (req, res) => {
+    
+    products.push(req.body)
+    res.redirect('/products')
+
+})
+
+
+// Products "destroy" route
+
+router.delete('/:id', (req,res)=>{
+
+    products.splice(req.params.id , 1 )
+    res.redirect('/products')
+})
+
+
+// Products "update" route
+
+router.put('/:id', (req,res)=>{
+  
+    products[req.params.id] = req.body
+    res.redirect(`/products/${req.params.id}`)
+
+})
+
+module.exports = router
+
--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express();
 const PORT = 5000;
 app.set ('view engine', 'ejs')
 
-//Simulated Database
-const products = require('./models/product_model')
+
+const productsController = require('./controllers/products_controller.js');
+app.use('/products', productsController);
+
+
 
 //Application static files configuration
 app.use(express.static('public'));
@@ -27,60 +30,7 @@ app.use((req,res,next) => {
 // registering methodOverride will allow us to add a query parameter called _method to our delete form
 app.use(methodOverride('_method'));
 
-//=============================================================================================
-                        //Routes/Controllers
-//=============================================================================================
 
-//Index Route - catches GET requests to /products/ and RENDERS ALL products
-app.get('/products/', (req,res) => {
-    const context = { products };
-    res.render('index.ejs', context);
-})
-
-app.get('/products/new', (req,res) => {
-    res.render('new.ejs');
-})
-
-
-//Show Route - catches GET requests to /products/index/ and RENDERS a SINGLE product
-app.get('/products/:id/', (req,res) => {
-    let productId = req.params.id
-    const context = {
-        oneProduct: products[productId],
-        message: "This is the show route"
-    }
-    res.render('show.ejs', context)
-})
-
-// 404 Error Catchall -- Put after all other routes!
-
-app.get("/*", (req,res) => {
-    const context = {error: req.error};
-    return res.status(404).render("404", context);
-});
-
-
-// ============================ POST Route =======================================
-
-    //Posts new product and redirects user to the products page. Lets individuals add their own kombuchas
-
-app.post('/products/', (req,res) => {
-    // console.log(req.body);
-    // res.send('Data Received: Check the terminal after clicking the button');  // Click button in /products/new and should return res.send();
-    const createdProduct = req.body
-    products.push(createdProduct)
-    // return res.send(`new product created: ${req.body.name}`);
-    return res.redirect('/products')
-});
-
-// ============================ Delete Route =======================================
-    // This route will catch DELETE requests to /products/anyValue and after deleting the data..
-    // respond by redirecting the user to the index route
-
-app.delete('/:productId', (req, res) => {
-    products.splice ( req. params.productId, 1);
-    return res.redirect ('/products');
-});
 
 
 //http://localhost:5000/products/1
