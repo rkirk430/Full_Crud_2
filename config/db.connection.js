c99d73d73594acd457f2342e074973d6e52a938b
rkirk430 <r.kirk430@gmail.com>
2022-05-09 08:11:05 -0400

    db.connection page set up

---
+++ config/db.connection.js
@@ -0,0 +1,37 @@
+//Steps to connect Express to Mongo
+
+// Step 1: mkdir config
+// Step 2: touch config/db.connection.js
+// Step 3: npm install mongoose dotenv
+
+
+// Step 4: Requiring a mongoose to get MongoDB Connection
+
+const mongoose = require ('mongoose');
+require('dotenv').config();  // Gives us access to .env
+
+// Step 5: Connecting Atlas with help of our .env file
+
+const connectionStr = process.env.MONGODB_URI;
+
+mongoose.connect(connectionStr);
+
+// Step 6: Registers if connection was successful
+
+mongoose.connnection.on('connected', () => {
+    console.log(`[${new Date().toLocaleTimeString()}]`)
+})
+
+
+// Step 7: Registers if connection was unsuccessful
+mongoose.connection.on('error', (error) => {
+    console.log('MongoDB connection error ', error)
+})
+
+
+// Step 8: Registers if Disconnecting from MongoDB
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected ⚡️ 🔌 ⚡️')
+})
+
+
